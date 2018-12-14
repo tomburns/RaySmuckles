289fa9699d6a5c18d77580a5f7bd24c0a7078983
Tom Burns <tomburns@gmail.com>
2018-12-13 22:06:13 -0600

    Initial Commit

---
+++ RaySmuckles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RaySmuckles
+//
+//  Created by Tom Burns on 12/13/18.
+//  Copyright © 2018 Tom Burns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RaySmucklesTests/RaySmucklesTests.swift
@@ -0,0 +1,34 @@
+//
+//  RaySmucklesTests.swift
+//  RaySmucklesTests
+//
+//  Created by Tom Burns on 12/13/18.
+//  Copyright © 2018 Tom Burns. All rights reserved.
+//
+
+import XCTest
+@testable import RaySmuckles
+
+class RaySmucklesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
